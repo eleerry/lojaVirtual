36e9950afcb7ffdd9b9da5bbd134e112a96952bf
eleerry <luisblack.lr@gmail.com>
2023-09-21 00:06:30 -0300

    atl

--- model/Config.class.php
+++ model/Config.class.php
@@ -5,14 +5,21 @@ class Config{
     const SITE_URL = "http://localhost",
           SITE_PASTA = "lojaVirtual",
           SITE_NOME = "Loja Eleerry",
-          SITE_EMAIL_ADM = "lojaeleerry@gmail.com";
+          SITE_EMAIL_ADM = "luisblack.lr@gmail.com";
 
     const BD_HOST = "localhost",
           BD_USER = "root",
           BD_SENHA = "",
           BD_BANCO = "lojaeleerry";
 
-    CONST EMAIL_HOST = "smtp.gmail.com";
+    CONST EMAIL_HOST = "smtp.gmail.com",
+          EMAIL_USER = "luisblack.lr@gmail.com",
+          EMAIL_NOME = "Contato Loja Eleérry",
+          EMAIL_SENHA = "lojadoeleerry",
+          EMAIL_PORTA = 587,
+          EMAIL_SMTPAUTH = true,
+          EMAIL_SMTPSECURETY = "tls",
+          EMAIL_COPIA = "luisblack.lr@gmail.com";
 }
 
 ?>
